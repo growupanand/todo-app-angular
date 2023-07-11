6ac14f0c8de72a1ce57273c91faa1c82c342ad9a
Utkarsh Anand <growupanand@gmail.com>
2023-07-11 23:56:04 +0530

    todos will now saved in localstorage of client browser

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -17,20 +17,34 @@ export class AppComponent {
   todoInputValue:string = "";
   lastId = 1;
 
+  constructor() {
+    const initialTodos = localStorage.getItem("todos");
+    this.todos = initialTodos ? JSON.parse(initialTodos) : [];
+    this.lastId = this.todos.length > 0 ? this.todos[this.todos.length - 1].id + 1 : 1;
+  }
+
   addTodo() {
-    this.todos.push({
+    const newTodo = {
       id : this.lastId,
       value : this.todoInputValue
-    });
+    };
+    this.todos.push(newTodo);
+    this.saveTodos();
     this.todoInputValue = "";
     this.lastId = this.lastId + 1 ;
   }
 
   deleteTodo(todoId:number) {
     this.todos = this.todos.filter(todo => todo.id !== todoId);
+    this.saveTodos();
   }
 
   emptyTodos() {
     this.todos = [];
+    localStorage.setItem("todos", JSON.stringify([]));
+  }
+
+  saveTodos() {
+    localStorage.setItem("todos", JSON.stringify(this.todos));
   }
 }
