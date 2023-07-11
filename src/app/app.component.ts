00cc484090ce2f8f2fdfb57c4e238b02897a5402
Utkarsh Anand <growupanand@gmail.com>
2023-07-11 23:33:25 +0530

    user can now delete an todo item or delete all items at once

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,16 +1,36 @@
 import { Component } from '@angular/core';
 
+
+
+type Todo = {
+  id : number;
+  value : string;
+}
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  todos:string[] = [];
+  todos:Todo[] = [];
   todoInputValue:string = "";
+  lastId = 1;
 
   addTodo() {
-    this.todos.push(this.todoInputValue);
+    this.todos.push({
+      id : this.lastId,
+      value : this.todoInputValue
+    });
     this.todoInputValue = "";
+    this.lastId = this.lastId + 1 ;
+  }
+
+  deleteTodo(todoId:number) {
+    this.todos = this.todos.filter(todo => todo.id !== todoId);
+  }
+
+  emptyTodos() {
+    this.todos = [];
   }
 }
